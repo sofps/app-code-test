221a9066dd35db18a9a07fdb51851ff785608b10
Sofía Pérez <sofia.perez.salvo@gmail.com>
2021-05-22 11:39:23 +0200

    Add HttpLoggingInterceptor to be able to see network requests and responses in logcat

--- android/CodeTest/app/src/main/java/com/codetest/main/di/AppModule.kt
+++ android/CodeTest/app/src/main/java/com/codetest/main/di/AppModule.kt
@@ -11,6 +11,7 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -24,6 +25,7 @@ internal object AppModule {
     @Provides
     fun provideOkHttpClient(): OkHttpClient =
         OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .build()
 
     @Provides
