78a750e18d8b39c52867d267116ecec07e213333
Sofía Pérez <sofia.perez.salvo@gmail.com>
2021-05-24 13:00:44 +0200

    Cleanup

--- android/CodeTest/app/src/main/java/com/codetest/CodeTestApplication.kt
+++ android/CodeTest/app/src/main/java/com/codetest/CodeTestApplication.kt
@@ -1,20 +1,7 @@
 package com.codetest
 
 import android.app.Application
-import android.content.Context
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class CodeTestApplication : Application() {
-    companion object {
-        private var context: Context? = null
-        fun appContext(): Context? {
-            return CodeTestApplication.context
-        }
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-        context = applicationContext
-    }
-}
+class CodeTestApplication : Application()
